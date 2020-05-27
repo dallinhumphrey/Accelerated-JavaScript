570296704b14891c19d3429ce65a0bb73bf66806
dallinhumphrey <dallinhumphrey@gmail.com>
2020-05-27 13:56:27 -0600

    121 setting up JS back end

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,7 @@
-import React, { Component } from 'react';
+"use strict";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Skeleton</h2>
-      </div>
-    );
-  }
+searchButton.addEventListener("click", searchWeather);
+
+function searchWeather() {
+  console.log(searchCity.value);
 }
