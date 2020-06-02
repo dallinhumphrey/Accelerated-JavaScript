4084f031be32e8f55f5c7e617ddb8b4f7e5b34a7
dallinhumphrey <dallinhumphrey@gmail.com>
2020-06-02 10:39:30 -0600

    124 fetching and handling weather data

--- src/components/app.js
+++ src/components/app.js
@@ -8,16 +8,27 @@ function searchWeather() {
     return alert("Please enter a city name.");
   }
   var http = new XMLHttpRequest();
-  var apiKey = "";
-  var url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName;
+  var apiKey = "c6651e7ac2399948c0ef5c7805ee2f6d";
+  var url =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&units=metric&appid=" +
+    apiKey;
   var method = "GET";
 
   http.open(method, url);
   http.onreadystatechange = function () {
     if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
       var data = JSON.parse(http.responseText);
+      var weatherData = new Weather(
+        cityName,
+        data.weather[0].description.toUpperCase()
+      );
+      weatherData.temperature = data.main.temp;
+      console.log(weatherData);
     } else if (http.readyState === XMLHttpRequest.DONE) {
       alert("Something went wrong!");
     }
   };
+  http.send();
 }
